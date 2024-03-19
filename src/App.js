fb4f228d28cdcc687282832ca5fbcb8d35392646
nipunraj <nipunraj@mozilor.com>
2024-03-19 09:11:33 +0530

    youer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: "GTM-WWBB26M",
+};
+TagManager.initialize(tagManagerArgs);
 
 function App() {
   return (
